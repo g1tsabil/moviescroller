4b6c4ef32c5689d0749f7ed93c1f6fb3e6a70e80
Sal Sabil <70616906+g1tsabil@users.noreply.github.com>
2022-08-07 23:53:41 -0400

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,89 @@
+$("#right").click(function() 
+{
+    $("#media").animate({scrollLeft: "+=1600px"}, "slow");
+});
+  
+$("#left").click(function() 
+{
+    $("#media").animate({scrollLeft: "-=1600px"}, "slow");
+});
+
+$("#bcs").click(function()
+{   
+    alert("Better Call Saul follows the transformation of Jimmy McGill, a former con artist who is trying to become a respectable lawyer.");
+});
+
+$("#morb").click(function()
+{   
+    alert("Dangerously ill with a rare blood disorder and determined to save others from the same fate, Dr. Morbius attempts a desperate gamble. While at first it seems to be a radical success, a darkness inside of him is soon unleashed.");
+});
+
+$("#sv").click(function()
+{   
+    alert("The series, a parody of Silicon Valley culture, focuses on Richard Hendricks, a programmer who founds a startup company called Pied Piper, and chronicles his struggles to maintain his company while facing competition from larger entities.");
+});
+
+$("#sop").click(function()
+{   
+    alert("The story revolves around fictional New Jersey-based, Italian American mobster Tony Soprano. The series portrays the difficulties that he faces as he tries to balance his home life and his criminal organization and his failure of never having the makings of a Varsity Athelete.");
+});
+
+$("#tp").click(function()
+{   
+    alert("The series follows an investigation, headed by FBI Special Agent Dale Cooper and local Sheriff Harry S. Truman, into the murder of homecoming queen Laura Palmer in the fictional town of Twin Peaks, Washington.");
+});
+
+$("#batman").click(function()
+{   
+    alert("Batman ventures into Gotham City's underworld when a sadistic killer leaves behind a trail of cryptic clues. As the evidence begins to lead closer to home and the scale of the perpetrator's plans become clear, he must forge new relationships, unmask the culprit and bring justice to the abuse of power and corruption that has long plagued the metropolis.");
+});
+
+$("#inter").click(function()
+{   
+    alert("In Earth's future, a global crop blight and second Dust Bowl are slowly rendering the planet uninhabitable. Professor Brand, a brilliant NASA physicist, is working on plans to save mankind by transporting Earth's population to a new home via a wormhole.");
+});
+
+$("#ds").click(function()
+{   
+    alert("Doctor Strange teams up with a mysterious teenage girl from his dreams who can travel across multiverses, to battle multiple threats, including other-universe versions of himself, which threaten to wipe out millions across the multiverse.");
+});
+
+$("#koth").click(function()
+{   
+    alert("Hank Hill, a propane gas salesman in the fictional town of Arlen, Texas, is often besieged by the idiosyncrasies of society, but finds some serenity in his home life with his wife, substitute Spanish teacher Peggy, his awkward son Bobby, and his live-in niece-in-law Luanne Platter.");
+});
+
+$("#bm").click(function()
+{   
+    alert("Set in a world only minutes from our own, Black Mirror, a UK and USA non-hosted anthology series; unveils how modern technologies can backfire and be used against their makers, every episode set in a slightly different reality with different characters combating different types of technologies.");
+});
+
+$("#topgun").click(function()
+{   
+    alert("After more than 30 years of service as one of the Navy's top aviators, Pete 'Maverick' Mitchell is where he belongs, pushing the envelope as a courageous test pilot and dodging the advancement in rank that would ground him.");
+});
+
+$("#jw").click(function()
+{   
+    alert("When sadistic mobster Iosef Tarasov and his thugs steal John's prized car and kill the puppy that was a last gift from his wife, John unleashes the remorseless killing machine within and seeks vengeance.");
+});
+
+$("#itcrowd").click(function()
+{   
+    alert("The comedic misadventures of Roy, Moss and their grifting supervisor Jen, a rag-tag team of IT support workers at a large corporation headed by a hotheaded yuppie.");
+});
+
+$("#from").click(function()
+{   
+    alert("In a nightmarish town in middle America that traps everyone who enters, unwilling residents strive to stay alive and search for a way out. But they are plagued by the terrifying nocturnal creatures from the surrounding forest.");
+});
+
+$("#bb").click(function()
+{   
+    alert("A high school chemistry teacher diagnosed with inoperable lung cancer turns to manufacturing and selling methamphetamine in order to secure his family's future.");
+});
+
+$("#min").click(function()
+{   
+    alert("In the 1970s, young Gru tries to join a group of supervillains called the Vicious 6 after they oust their leader, the legendary fighter Wild Knuckles.");
+});
